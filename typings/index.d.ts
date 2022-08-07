49fea26e7193409760cdbe1006f0dbae96d24a6c
Joseph Perez <joseph@p14.dev>
2022-08-06 21:49:25 -0700

    dependencies

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -10,6 +10,8 @@ declare global {
 }
 
 declare module 'cors';
+declare module 'bcrypt';
+declare module 'jsonwebtoken';
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
