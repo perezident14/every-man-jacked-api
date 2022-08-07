587640a3b6c7e7d2e4ed519cbf3a438bf02aac21
Joseph Perez <joseph@p14.dev>
2022-08-07 08:13:03 -0700

    dependencies

--- src/index.d.ts
+++ src/index.d.ts
@@ -13,6 +13,4 @@ declare module 'cors';
 declare module 'bcrypt';
 declare module 'jsonwebtoken';
 
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
 export { }
