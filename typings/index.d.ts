50ec3022847591fb05d5f7c09f13e3f63d96b85d
Joseph Perez <joseph@p14.dev>
2022-08-06 21:44:05 -0700

    npm start

---
+++ typings/index.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      DB_URI: string;
+      JWT_SECRET: string;
+      SALT_ROUNDS: string;
+    }
+  }
+}
+
+declare module 'cors';
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export { }
