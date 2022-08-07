cca58b4094283ad48e9c9d13a70be5a389d5c40e
Joseph Perez <joseph@p14.dev>
2022-08-06 23:43:39 -0700

    origin updates

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,13 @@ const cors = require('cors');
 
 const corsOptions = {
   credentials: true,
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://everymanjacked.com'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://everymanjacked.com',
+    'https://www.everymanjacked.com',
+    'https://admin.everymanjacked.com',
+  ],
 };
 
 const app = express();
